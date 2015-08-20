77e36bb29de7aef4444c2c239ac719d121fde2a5
Jack Moorhouse <moverperfect@gmail.com>
2015-08-21 00:03:41 +0100

    Added base for the add new video page

---
+++ Track Video Watching/AddNewVideo.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Track_Video_Watching
+{
+    public partial class AddNewVideo : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Calendar1.TodaysDate = DateTime.Today;
+            Calendar1.SelectedDate = DateTime.Today;
+        }
+
+        protected void cboPlatform_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtPlatform.Visible = cboPlatform.Text == "Other";
+        }
+    }
+}
