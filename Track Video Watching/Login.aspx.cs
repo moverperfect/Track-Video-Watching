12ab632ea07f40a38dc4a04f7f42359696a60bda
Jack Moorhouse <moverperfect@gmail.com>
2015-08-20 23:21:36 +0100

    Added the ability to log in with tokens stored in cookies

--- Track Video Watching/Login.aspx.cs
+++ Track Video Watching/Login.aspx.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Track_Video_Watching
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class Login : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,7 +26,32 @@ namespace Track_Video_Watching
         /// </summary>
         protected void cmdLogin_Click(object sender, EventArgs e)
         {
+            var mysql = new SqlConnector("db_trackvideowatching");
 
+            var users =
+                (DataTable)
+                    mysql.Select("SELECT Username, Password_Hash, Salt FROM tbl_Users WHERE Username = '" +
+                                 txtUsername.Text + "';");
+            if (users.Rows.Count != 0)
+            {
+                var matchHash = Utilities.HashPassword(txtPassword.Text, users.Rows[0][2].ToString(), MD5.Create());
+                if (matchHash == users.Rows[0][1].ToString())
+                {
+                    var coookieValue = Utilities.HashPassword(txtUsername.Text + users.Rows[0][1].ToString(), users.Rows[0][2].ToString(), 
+                        MD5.Create());
+                    Response.Cookies.Add(new HttpCookie("Username", txtUsername.Text));
+                    Response.Cookies.Add(new HttpCookie("Token", coookieValue));
+                    Response.Redirect("Dashboard.aspx");
+                }
+                else
+                {
+                    Response.Write("Incorrect password");
+                }
+            }
+            else
+            {
+                Response.Write("No such user exists");
+            }
         }
     }
 }
