e569bfde6dc69c95bb61863fc2372560e8d0eeab
Jack Moorhouse <moverperfect@gmail.com>
2015-08-25 02:15:33 +0100

    Started to add statistics page

---
+++ Track Video Watching/Statistics.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Statistics.aspx.cs" Inherits="Track_Video_Watching.Statistics" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="btnShowMinPlat" runat="server" OnClick="btnShowMinPlat_Click" Text="Show minutes by video platform" />
+        <br />
+        <asp:Chart ID="graMinPlat" runat="server" Height="264px" Width="272px">
+            <series>
+                <asp:Series ChartType="Pie" Name="Series1">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="ChartArea1">
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Track Video Watching/Statistics.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+
+namespace Track_Video_Watching
+{
+    public partial class Statistics : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShowMinPlat_Click(object sender, EventArgs e)
+        {
+            var mysql = new SqlConnector("db_trackvideowatching");
+
+            var platMin = (DataTable) mysql.Select("SELECT Video_Platform, Length FROM tbl_records GROUP BY Video_Platform;");
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Platform");
+            dt.Columns.Add("Total");
+
+            for (int i = 0; i < platMin.Rows.Count; i++)
+            {
+                dt.Rows.Add(platMin.Rows[i][0], ((TimeSpan) platMin.Rows[i][1]).Seconds);
+            }
+
+            graMinPlat.DataSource = dt;
+            graMinPlat.Series["Series1"].XValueMember = "Platform";
+            graMinPlat.Series["Series1"].YValueMembers = "Total";
+            graMinPlat.Series["Series1"].Label = "#VALX #PERCENT{P0}";
+            graMinPlat.DataBind();
+        }
+    }
+}
